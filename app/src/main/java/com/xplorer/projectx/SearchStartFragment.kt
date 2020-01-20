2eca06d89a5d14a7bfbf714879cd04c681e6f728
Kola Emiola <kolaemiola@Andelas-MacBook-Pro.local>
2020-01-20 16:24:38 +0100

    XA-16: updated build key for google api key

--- app/src/main/java/com/xplorer/projectx/SearchStartFragment.kt
+++ app/src/main/java/com/xplorer/projectx/SearchStartFragment.kt
@@ -10,9 +10,11 @@ import com.google.android.gms.common.api.Status
 import com.google.android.libraries.places.api.Places
 import com.xplorer.projectx.databinding.FragmentSearchStartBinding
 import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.TypeFilter
 import com.google.android.libraries.places.api.net.PlacesClient
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.xplorer.projectx.BuildConfig.GOOGLE_API_KEY
 
 /**
  * A simple [Fragment] subclass.
@@ -35,7 +37,7 @@ class SearchStartFragment : Fragment() {
 
     private fun getPlaceAutocomplete() {
         if (!Places.isInitialized()) {
-            Places.initialize(context!!, getString(R.string.google_api_key))
+            Places.initialize(context!!, GOOGLE_API_KEY)
         }
         placesClients = Places.createClient(context!!)
         // Initialize the AutocompleteSupportFragment.
@@ -43,6 +45,7 @@ class SearchStartFragment : Fragment() {
             findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
         // Specify the types of place data to return.
         autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS))
+        autocompleteFragment.setTypeFilter(TypeFilter.CITIES)
 
         // Set up a PlaceSelectionListener to handle the response.
         autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
