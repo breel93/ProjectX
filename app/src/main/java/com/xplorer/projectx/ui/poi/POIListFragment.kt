658aaff97db1d39a1febbf8cb15735c7ee614f3f
Chukwukammadu Anizoba Robert <omega.help008@gmail.com>
2020-03-05 15:43:00 +0100

    XA-44- Create recommended places of interest list fragment, add fragment to navigation graph

--- app/src/main/java/com/xplorer/projectx/ui/poi/POIListFragment.kt
+++ app/src/main/java/com/xplorer/projectx/ui/poi/POIListFragment.kt
@@ -19,15 +19,70 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dagger.android.support.DaggerFragment
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.recyclerview.widget.RecyclerView
+import com.xplorer.projectx.R
+import com.xplorer.projectx.databinding.PlacesListContainerBinding
+import com.xplorer.projectx.model.foursquare.Venue
+import com.xplorer.projectx.model.foursquare.VenueLocation
+import com.xplorer.projectx.ui.adapter.poi.venues.VenueListAdapter
+import com.xplorer.projectx.ui.adapter.snap.SnapOnScrollListener
 
-class POIListFragment : DaggerFragment() {
+class POIListFragment : Fragment() {
+
+  private lateinit var binding: PlacesListContainerBinding
+  private val venueList = ArrayList<Venue>()
+  private lateinit var venueListAdapter: VenueListAdapter
+  private lateinit var venueRecycler: RecyclerView
+  private val snapHelper = LinearSnapHelper()
 
   override fun onCreateView(
     inflater: LayoutInflater,
     container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View? {
-    return super.onCreateView(inflater, container, savedInstanceState)
+
+    binding = DataBindingUtil.inflate(LayoutInflater.from(context),
+      R.layout.places_list_container,
+      container,
+      false)
+
+    val venue1 = Venue("",
+      "The Place",
+      VenueLocation("South Side", 0f, 0f))
+
+    for (x in 0 until 10) {
+      venueList.add(venue1)
+    }
+
+    venueRecycler = binding.placeListRecycler
+
+    venueListAdapter = VenueListAdapter(context!!, venueList) { selectedVenue ->
+      Toast.makeText(context,
+        "Clicked on venue: ${selectedVenue.venueName}",
+        Toast.LENGTH_SHORT)
+        .show()
+    }
+
+    venueRecycler.layoutManager = LinearLayoutManager(context,
+      LinearLayoutManager.HORIZONTAL,
+      false)
+
+    venueRecycler.adapter = venueListAdapter
+    snapHelper.attachToRecyclerView(venueRecycler)
+
+    val snapOnScrollListener = SnapOnScrollListener(snapHelper,
+      SnapOnScrollListener.NotifyBehavior.NOTIFY_ON_STATE_IDLE
+    ) { snapPosition ->
+      Toast.makeText(context, "Snapped at position: $snapPosition", Toast.LENGTH_SHORT).show()
+    }
+
+    venueRecycler.addOnScrollListener(snapOnScrollListener)
+
+    return binding.root
   }
 }
