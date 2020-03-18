c6ef37def84316c83aadd8aef042e7fa946d223e
Chukwukammadu Anizoba Robert <omega.help008@gmail.com>
2020-03-18 16:13:04 +0100

    XA-Refactor Apply spotless to files

--- app/src/main/java/com/xplorer/projectx/utils/BrowserUtils.kt
+++ app/src/main/java/com/xplorer/projectx/utils/BrowserUtils.kt
@@ -1,3 +1,18 @@
+/**
+ *  Designed and developed by ProjectX
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
 package com.xplorer.projectx.utils
 
 import android.content.Context
@@ -7,9 +22,11 @@ import androidx.browser.customtabs.CustomTabsIntent
 class BrowserUtils {
 
   companion object {
-    fun launchBrowser(context: Context,
-                      url: String,
-                      onWebView: (() -> Unit)? = null) {
+    fun launchBrowser(
+      context: Context,
+      url: String,
+      onWebView: (() -> Unit)? = null
+    ) {
 
       val hasChrome = AppPackageUtils.appInstalled(context, "com.android.chrome")
       if (hasChrome) {
